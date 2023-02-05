e53b350cae31521ba86207b91d6ae28714e46dd0
Medved Dima <dimam@assuta.co.il>
2023-02-05 16:03:07 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import Navbar from "@/scenes/navbar/Navbar";
+import Home from "@/scenes/home/Home";
+import Benefits from "@/scenes/benefits/Benefits";
+import OurClasses from "@/scenes/our-classes/OurClasses";
+import ContactUs from "@/scenes/contact-us/ContactUs";
+import Footer from "@/scenes/footer/Footer";
+
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
